e65d0626a4f88abbf842ab4edb339c3e1c91f1dc
franzmoro <franzmoro@hotmail.com>
2016-02-04 15:21:14 +0000

    multiple queries cloudsound.js

--- cloudsound.js
+++ cloudsound.js
@@ -7,7 +7,7 @@ var world  = document.getElementById("world");
 
 // event listeners
 var weatherQuery = {
-    "clear sky":['jazz'],
+    "clear sky": {tags:["sunny", "sun", "sunshine", "hot", "warm", "bright", "cloudless"]},
     "few clouds":[""],
     "scattered clouds":[],
     "broken clouds":[],
@@ -25,87 +25,39 @@ SC.initialize({
   client_id: secretKeys.soundcloud || prompt("soundcloud user key please")
 });
 
-function getTracks(tags){
-    var secretKey = secretKeys.soundcloud || prompt("soundcloud user key please");
-    var url = 'https://api.soundcloud.com/tracks?client_id='+secretKey+'&genres='+tags;
-    var xhr = new XMLHttpRequest();
-
-    // xhr.onreadystatechange = function() {
-    //     // if(xhr.readyState === 4 && xhr.status === 200) {
-    //
-    //     // }
-    // };
-    xhr.open('GET',url,false);
-    xhr.send();
-    return JSON.parse(xhr.responseText);
-}
-
-function cloudQuery(weatherResult){
-    var res = [];
-    for (var meteo in weatherQuery) {
-        if (meteo === weatherResult) {
-            var tags = weatherQuery[meteo];
-            for (var i = 0; i<tags.length; i++) {
-                res = res.concat(getTracks(tags[i]));
+function getTracks(weather){
+    var query     = weatherQuery[weather];
+    var allTracks = [];
+    for (var i = 0; i<query.tags.length; i++) {
+        var tag = query.tags[i];
+        console.log(tag);
+        SC.get('/tracks',{
+            q:  tag
+        }).then(function(tracks){
+                    // pushh all tracks to allTracks
+            for(j=0;j<tracks.length;j++){
+                allTracks.push(tracks[i]);
             }
-        }
+        });
     }
-    return res;
+    return allTracks;
 }
+
+
 
 // WARNING needs to run based on weather result!!
 
 function play(songObj){
-    var trackUrl = songObj.uri.replace("https://api.soundcloud.com","")+'/';
+    var trackId = songObj.id;
+    var trackUrl = '/tracks/'+trackId;
+    console.log(trackUrl);
     SC.stream(trackUrl).then(function(player){
       player.play();
     });
-    
+
     // SC.stream(trackUrl, function(sound) {
     //     SC.sound = sound;
     // });
     // SC.sound.play();
 
 }
-
-function resume(){
-    // necessary??
-    var current = '/tracks/219980056/';
-    play(current);
-}
-
-
-
-
-
-
-// // find all sounds of buskers licensed under 'creative commons share alike'
-// SC.get('/tracks', {
-//   q: 'buskers', license: 'cc-by-sa'
-// }).then(function(tracks) {
-//     var track1 = tracks[0];
-//     console.log(track1);
-// });
-
-
-// //search bpm
-//
-// SC.initialize({
-//   client_id: 'YOUR_CLIENT_ID'
-// });
-//
-// // find all tracks with the genre 'punk' that have a tempo greater than 120 bpm.
-// SC.get('/tracks', {
-//   genres: 'punk', bpm: { from: 120 }
-// }).then(function(tracks) {
-//   console.log(tracks);
-// });
-
-
-// // player no widget
-//
-// SC.initialize({
-//   client_id: 'YOUR_CLIENT_ID'
-// });
-//
-// // stream track id 293
